35e2f688c6793fa88d9932277dbe98aa844f1c8c
valentinab25 <valentinab25>
2018-09-10 17:52:58 +0300

    Update installed packages  Refs #94921

--- docker-setup.sh
+++ docker-setup.sh
@@ -21,7 +21,7 @@ runDeps="
   libexpat1-dev
   libjpeg-dev
   libjpeg62
-  libmagickcore-6.q16-2-extra
+  libmagickcore-6.q16-3-extra
   libmemcached-dev
   libpq-dev
   libreadline-dev
@@ -46,11 +46,12 @@ runDeps="
   wv
   xpdf
   zlib1g-dev
-  ttf-mscorefonts-installer
   calibre
   xvfb
   imagemagick
   libpng16-16
+  apt-transport-https
+  lsb-release
 "
 
 echo "========================================================================="
@@ -65,56 +66,19 @@ echo "========================================================================="
 echo "Installing NodeJS"
 echo "========================================================================="
 
-curl -sL https://deb.nodesource.com/setup_10.x | bash
-apt install nodejs
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo 'deb https://deb.nodesource.com/node_10.x stretch main' > /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src https://deb.nodesource.com/node_10.x stretch main' >> /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt install -y --no-install-recommends nodejs
 
 rm -rf /var/lib/apt/lists/*
 
 echo "========================================================================="
-echo "Installing wkhtmltopdf 0.12.2.4"
+echo "Installing wkhtmltopdf 0.12.2"
 echo "========================================================================="
 
-curl -o /tmp/wkhtmltopdf-0.12.2.4.tgz -SL https://svn.eionet.europa.eu/repositories/Zope/trunk/wk/wkhtmltopdf-0.12.2.4.tgz
-echo "9482532471f61084797a61b57d83eb85 /tmp/wkhtmltopdf-0.12.2.4.tgz" | md5sum -c -
-tar -zxvf /tmp/wkhtmltopdf-0.12.2.4.tgz -C /usr/bin/
-rm -vf /tmp/wkhtmltopdf-0.12.2.4.tgz
+curl -o /tmp/wkhtmltopdf.tgz -SL https://svn.eionet.europa.eu/repositories/Zope/trunk/wk/wkhtmltopdf-0.12.4.tgz
+tar -zxvf /tmp/wkhtmltopdf.tgz -C /tmp/
+mv -v /tmp/wkhtmltopdf /usr/bin/
 
-echo "========================================================================="
-echo "Fix zlib path"
-echo "========================================================================="
-
-ln -s /lib/x86_64-linux-gnu/libz.so.1 /lib/libz.so
-
-echo "========================================================================="
-echo "Installing Python 2.4"
-echo "========================================================================="
-
-cd /tmp
-curl -o Python-2.4.6.tgz -SL https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tgz
-echo "7564b2b142b1b8345cd5358b7aaaa482 /tmp/Python-2.4.6.tgz" | md5sum -c -
-tar -zxvf Python-2.4.6.tgz
-cd Python-2.4.6
-./configure
-make
-make install
-rm -rf /tmp/Python*
-
-echo "========================================================================="
-echo "Installing Python 2.6"
-echo "========================================================================="
-
-cd /tmp
-curl -o Python-2.6.9.tgz -SL https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz
-echo "bddbd64bf6f5344fc55bbe49a72fe4f3 /tmp/Python-2.6.9.tgz" | md5sum -c -
-tar -zxvf Python-2.6.9.tgz
-cd Python-2.6.9
-./configure
-make
-make install
-rm -rf /tmp/Python*
-
-echo "========================================================================="
-echo "Fixing default python"
-echo "========================================================================="
-
-mv /usr/local/bin/python /usr/local/bin/python-2.6
